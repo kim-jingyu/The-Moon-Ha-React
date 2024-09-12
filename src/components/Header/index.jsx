b513c167d2484931d6a96b5a8f20687849a15bc1
yookyung Choi <ykc131719@gmail.com>
2024-09-13 08:35:21 +0900

    [KAN-11] Feat: 로그아웃 구현

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,12 +5,33 @@ import logo from '../../assets/images/logo.svg';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { authState } from '../../recoil';
 import Button from '../Button';
+import { LogOutAPI } from '../../apis/Auth';
+import { notification } from 'antd';
+import { useNavigate } from 'react-router';
 
 const Header = () => {
+    const navigate = useNavigate();
     const isAuthenticated = useRecoilValue(authState);
     const setAuthState = useSetRecoilState(authState);
 
-    const handleLogout = () => {
+    const handleLogout = async () => {
+        try {
+            // 로그인 API 요청
+            const response = await LogOutAPI();
+            console.log('LogOutAPI response : ', response);
+            if (response.data.success) {
+                // 로그인 성공 시 localStorage에 저장
+                localStorage.setItem('isLoggedIn', 'false');
+                setAuthState(false);
+                notification.success({ message: 'Logout successful!' }); // 성공 알림
+                navigate('/login');
+            } else {
+                notification.error({ message: 'Login failed. Please try again.' }); // 실패 알림
+            }
+        } catch (error) {
+            console.error('LogOutAPI error:', error);
+            notification.error({ message: 'An error occurred. Please try again.' }); // 오류 알림
+        }
         setAuthState(false);
     };
 
