04d9c786f208de4e07cf786accdaa3d9e0bbc89b
yookyung Choi <ykc131719@gmail.com>
2024-09-21 16:04:10 +0900

    [KAN-60] Refactor: 통신 timeout 늘리기

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import { getCookie } from '../utils/cookie';
 const instance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
-    timeout: 1000,
+    timeout: 10000,
     withCredentials: true,
     headers: {
         Accept: 'application/json',
