ff7c6568c07461440fc632a32690ef36e2d7882a
kim-jingyu <swc05161@gmail.com>
2024-09-23 10:32:35 +0900

    [KAN-44] Style: 주석 추가

--- src/pages/LiveStartPage/index.jsx
+++ src/pages/LiveStartPage/index.jsx
@@ -1,3 +1,16 @@
+/**
+ * 실시간 강좌 시작 페이지
+ * @author 김진규
+ * @since 2024.09.09
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.09.09  	김진규       최초 생성
+ * </pre>
+ */
+
 import React from 'react';
 import { BodyWrapper, Container, TitleWrapper } from './styled';
 import LiveStartComponent from '../../components/LiveStartComponent';
